4ce2afe358a452896c2e70a0614f2e945b58b732
Vadym <avsimvadim@gmail.com>
2018-09-27 23:37:36 +0300

    first commit

---
+++ src/kiev/Main.java
@@ -0,0 +1,8 @@
+package kiev;
+//your task will be to sort this array from min to max [4,3,5,34,5,6,3,6,0,3,-3,-57,3333]
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
