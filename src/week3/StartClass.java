551962aa8994fad198ca2344e33496483b75ddc5
Vadym <avsimvadim@gmail.com>
2019-03-18 21:36:06 +0200

    3/18/2019 week3 homework for 2 days

---
+++ src/week3/StartClass.java
@@ -0,0 +1,14 @@
+package week3;
+
+//you have the String array String[] s =
+//                          = {"abc", "sfscsvadimfsc", "dh3vadiovn", "8vadimnvb8""abcdvd","afbc"};
+//you have to create class which contains method that can find the index of string with a name "vadim"
+//on the main class show a result
+//you any have to create a method in the class which makes any word in a reverse order
+//for example these words "  abcd  ", "    serhii"
+//result in main
+public class StartClass {
+    public static void main(String[] args) {
+
+    }
+}
